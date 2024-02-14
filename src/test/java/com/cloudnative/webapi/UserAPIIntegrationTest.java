ef688dd3943920e44e1e64a4a5431d9e1d0bd31c
nimje.p@northeastern.edu <nimje.p@northeastern.edu>
2024-02-14 13:37:53 -0500

    Removed Jmeter scripts and added rest assured for integration testing

---
+++ src/test/java/com/cloudnative/webapi/UserAPIIntegrationTest.java
@@ -0,0 +1,122 @@
+package com.cloudnative.webapi;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import net.minidev.json.JSONObject;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class UserAPIIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    private static String testUserName;
+
+    @BeforeEach
+    public void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    @BeforeAll
+    public static void init() {
+        LocalDateTime now = LocalDateTime.now();
+        String datetimeStr = now.format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
+        testUserName = String.format("user_%s@email.com", datetimeStr);
+    }
+
+    @Test
+    @Order(1)
+    public void testCreateAndValidateUser() {
+        String username = testUserName;
+        String password = "Password@123";
+
+        JSONObject newUser = new JSONObject();
+        newUser.put("username", username);
+        newUser.put("password", password);
+        newUser.put("first_name", "TestFirstname");
+        newUser.put("last_name", "TestLastname");
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(newUser.toJSONString())
+                .when()
+                .post("/v1/user")
+                .then()
+                .statusCode(201)
+                .body("id", notNullValue())
+                .body("username", equalTo(username))
+                .extract()
+                .response();
+
+        String id = response.jsonPath().getString("id");
+        assertDoesNotThrow(() -> UUID.fromString(id), "Invalid UUID");
+
+        getUserAndVerify(username, password)
+                .body("id", notNullValue())
+                .body("id", equalTo(id))
+                .body("username", equalTo(username));
+    }
+
+    @Test
+    @Order(2)
+    public void testUpdateAndValidateUser() {
+        String username = testUserName;
+        String currentPassword = "Password@123";
+        String updatedPassword = "NewPassword@123";
+        String updatedFirstname = "TestNewFirstname";
+
+        JSONObject newUser = new JSONObject();
+        newUser.put("password", updatedPassword);
+        newUser.put("first_name", updatedFirstname);
+
+        given()
+                .auth()
+                .basic(username, currentPassword)
+                .contentType(ContentType.JSON)
+                .body(newUser.toJSONString())
+                .when()
+                .put("/v1/user/self")
+                .then()
+                .statusCode(204);
+
+        getUserAndVerify(username, updatedPassword)
+                .body("id", notNullValue())
+                .body("username", equalTo(username))
+                .body("first_name", equalTo(updatedFirstname))
+                .body("last_name", equalTo("TestLastname"));
+    }
+
+    private ValidatableResponse getUserAndVerify(String username, String password) {
+        return given()
+                .auth()
+                .basic(username, password)
+                .when()
+                .get("/v1/user/self")
+                .then()
+                .statusCode(200);
+    }
+}
