ae2a87d55c09fe158aa5c0884e02b79e03b963c3
nimje.p@northeastern.edu <nimje.p@northeastern.edu>
2024-04-03 12:04:43 -0400

    Changed response status for email verification pending response

--- src/main/java/com/cloudnative/webapi/config/auth/BasicAuthEntryPoint.java
+++ src/main/java/com/cloudnative/webapi/config/auth/BasicAuthEntryPoint.java
@@ -28,26 +28,29 @@ public class BasicAuthEntryPoint extends BasicAuthenticationEntryPoint {
                          HttpServletResponse response,
                          AuthenticationException authException)
             throws IOException {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setHeader("WWW-Authenticate", "Basic realm=\"" + this.getRealmName() + "\"");
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
 
         ObjectMapper mapper = new ObjectMapper();
-        ApiResponse authResponse = getAuthResponse(authException);
+        ApiResponse authResponse = getAuthResponse(authException, response);
         String jsonPayload = mapper.writeValueAsString(authResponse);
         response.getWriter().write(jsonPayload);
     }
 
-    private ApiResponse getAuthResponse(AuthenticationException authException) {
+    private ApiResponse getAuthResponse(AuthenticationException authException, HttpServletResponse response) {
         ApiResponse authResponse;
         if (authException instanceof BadCredentialsException && authException.getMessage().equalsIgnoreCase("Email is not verified")) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
             authResponse = new ApiResponse(false, "Email verification is pending");
         } else if (authException instanceof BadCredentialsException) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             authResponse = new ApiResponse(false, "Invalid username or password");
         } else if (authException.getMessage().equalsIgnoreCase("Full authentication is required to access this resource")) { // Default message for missing Authorization header
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             authResponse = new ApiResponse(false, "Authorization header is missing");
         } else {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             authResponse = new ApiResponse(false, "Internal server error");
         }
         logger.warn("User authentication failed: {}", authResponse.getMessage());
